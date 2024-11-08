82a686be127f6a76185229e5563e0b86dba3b209
SogoHlopec <sogo246@gmail.com>
2024-11-09 02:22:12 +0300

    feat: add a solution for Trigger depth limit rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -18,6 +19,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       const body = req.body;
       const query = body.query;
       const variables = body.variables;
+
+      const parsedQuery = parse(query);
+      const validationErrors = validate(schema, parsedQuery, [depthLimit(5)]);
+
+      if (validationErrors.length > 0) {
+        return {
+          errors: validationErrors.map((error) => ({
+            message: error.message,
+            locations: error.locations,
+          })),
+        };
+      }
 
       const result = await graphql({
         schema: schema,
